7568ad2e7cf0658c4491d9ff0cbc638c309e8b34
shivaprasad-bhat <shivaprasad@svbneelmane.in>
2022-08-14 13:11:34 +0530

    Rearrange the code

--- src/main/kotlin/bhat/svbneelmane/coursecatalogservice/controllers/CourseController.kt
+++ src/main/kotlin/bhat/svbneelmane/coursecatalogservice/controllers/CourseController.kt
@@ -3,19 +3,14 @@ package bhat.svbneelmane.coursecatalogservice.controllers
 import bhat.svbneelmane.coursecatalogservice.dto.CourseDTO
 import bhat.svbneelmane.coursecatalogservice.services.CourseService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/v1/courses")
 class CourseController(val courseService: CourseService) {
 
     /**
-     * Request - [POST]
+     * Request - POST
      * Controller function that adds a new course to database
      */
     @PostMapping
